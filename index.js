867018a2b0626729ed6ef49f840df7d2fb13c554
MVAodhan <hamilton.aodhan@gmail.com>
2022-10-09 18:37:25 +1300

    working with multiple posts

--- index.js
+++ index.js
@@ -15,24 +15,10 @@ const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 const uuid = require('uuid').v4;
 const sharp = require('sharp');
 
-// const bucketName = process.env.AWS_BUCKET_NAME;
-const region = process.env.AWS_BUCKET_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_KEY;
-
 const bucketName = process.env.CLOUDFLARE_BUCKET_NAME;
 const cloudflareEndpoint = process.env.CLOUDFLARE_API_ENDPOINT;
 const cloudflareAccessKey = process.env.CLOUDFLARE_ACCESS_KEY;
 const cloudflareSecretKey = process.env.CLOUDFLARE_SECRET_KEY;
-
-// Connecting to s3 bucket
-// const s3 = new S3Client({
-//   region,
-//   credentials: {
-//     accessKeyId,
-//     secretAccessKey,
-//   },
-// });
 
 // connecting to cloudflare r2 with s3 sdk
 const s3 = new S3Client({
@@ -48,45 +34,11 @@ const randomImageName = (origionalname) => {
   return name;
 };
 
-// storing in a single location wi no options, check line 33 for implimentation with options
-// const upload = multer({ dest: './uploads/' });
-
-var corsOptions = {
+const corsOptions = {
   origin: 'http://localhost:3000',
 };
 const app = express();
 app.use(cors(corsOptions));
-
-//single file upload
-// app.post('/upload', upload.single('file'), (req, res) => {
-//   res.json({ status: 'success' });
-// });
-
-// multiple file upload
-// app.post('/upload', upload.array('file', 2), (req, res) => {
-//   res.json({ status: 'success' });
-// });
-
-//multiple fields
-// const multiUpload = upload.fields([
-//   { name: 'avatar', maxCount: 1 },
-//   { name: 'resume', maxCount: 1 },
-// ]);
-
-// app.post('/upload', multiUpload, (req, res) => {
-//   res.json({ status: 'success' });
-// });
-
-// Store File on disk in node server
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, 'uploads');
-//   },
-//   filename: function (req, file, cb) {
-//     const { originalname } = file;
-//     cb(null, `${uuid()}-${originalname}`);
-//   },
-// });
 
 // Store file in memory to transform and validate before sending to S3
 const storage = multer.memoryStorage();
@@ -103,40 +55,48 @@ const upload = multer({
   fileFilter,
   // limits: { fileSize: 70000, files: 2 },
 });
-
-// app.get('/', async (req, res) => {
-//   const getObjectParams = {
-//     Bucket: bucketName,
-//     Key: '',
-//   };
-//   const command = new GetObjectCommand(getObjectParams);
-//   const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
-//   res.send(JSON.stringify('Hello express multer'));
-// });
+const getSignedObject = async (key) => {
+  const getObjectParams = {
+    Bucket: bucketName,
+    Key: key,
+  };
+  const command = new GetObjectCommand(getObjectParams);
+  const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+  return url;
+};
 
 app.get('/', async (req, res) => {
-  res.json({
-    message: 'server active',
-  });
+  const posts = await prisma.posts.findMany();
+  for (const post of posts) {
+    post.imageName = await getSignedObject(post.imageName);
+  }
+  res.send(posts);
 });
 
 app.post('/upload', upload.single('image'), async (req, res) => {
   const file = req.file;
   const caption = req.body.caption;
-  console.log(file);
-  console.log(caption);
-  // const transformedBuffer = await sharp(req.file.buffer)
-  //   .resize(1080, 1920, { fit: 'contain' })
-  //   .toBuffer();
-  // const params = {
-  //   Bucket: bucketName,
-  //   Key: randomImageName(req.file.originalname),
-  //   Body: transformedBuffer,
-  //   ContentType: req.file.mimetype,
-  // };
-  // const command = new PutObjectCommand(params);
-  // await s3.send(command);
-  // res.json({ message: 'success' });
+  const imageName = randomImageName(file.originalname);
+  const fileBuffer = await sharp(file.buffer)
+    .resize(1080, 1920, { fit: 'contain' })
+    .toBuffer();
+  const params = {
+    Bucket: bucketName,
+    Key: imageName,
+    Body: fileBuffer,
+    ContentType: file.mimetype,
+  };
+  const command = new PutObjectCommand(params);
+  await s3.send(command);
+
+  const post = await prisma.posts.create({
+    data: {
+      imageName,
+      caption,
+    },
+  });
+
+  res.json({ message: 'success' });
 });
 
 app.use((err, req, res, next) => {
